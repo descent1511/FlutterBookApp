645bf173968673a8d12de9d8212ad23951f636c4
Mesh296 <minhhan181@gmail.com>
2024-04-19 18:40:02 +0300

    feat: create login and register page

---
+++ app/lib/loginPage.dart
@@ -0,0 +1,68 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  String _username = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              onChanged: (value) {
+                _username = value;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                prefixIcon: Icon(Icons.person), 
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              onChanged: (value) {
+                _password = value;
+              },
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.lock), 
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint("Login");
+                debugPrint("Username: " + this._username);
+                debugPrint("Password: " + this._password);
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: const Text('Sign in'),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have account?"),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/signup');
+                    },
+                    child: const Text("Sign up"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
